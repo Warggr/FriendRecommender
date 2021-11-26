5816c23f677fba3c6a20b54cfe300631dcf67058
Pierre Ballif <pierre.ballif@tum.de>
2021-11-25 19:03:06 -0500

    fix some-ids.sh

--- some-ids.sh
+++ some-ids.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 for i in 924 8941 8942 9019 9020 9021 9022 9990 9992 9993; do
-	cat output/second-mapper/part-r-00000 | grep -e "^$i";
+	cat output/second-mapper/part-r-00000 | grep -e "^$i\s";
 done
